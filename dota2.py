2c11441301910683b15679f45e123f2e69518ef8
Arbueba <142947815+Arbueba@users.noreply.github.com>
2023-08-23 09:32:58 +1000

    Add files via upload

---
+++ dota2.py
@@ -0,0 +1,17 @@
+print('Please enter top 5 popular Dota 2 characters')
+user = input()
+
+if user == ('Pudge'):
+    print('An interesting character who makes a lot of problems for your team')
+elif user == ('Legion Commander'):
+    print('With each duel won, she hits harder and by the end of the battle she becomes an invincible warrior.')
+elif user == ('Witch Doctor'):
+    print('He can gradually heal comrades, stun and curse enemies to death!')
+elif user == ('Lion'):
+    print('It stuns the victim with stone spikes and temporarily turns it into a harmless animal')
+elif user == ('Phantom Assassin'):
+    print('Blow after blow, any of which can be fatal!')
+else:
+    print("Hmm... is this a new character???")
+
+input() 
